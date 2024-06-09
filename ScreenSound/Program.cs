ab522e1506885abfd70edfaf4e2694cb0f779e98
Luka Fernandes <150366532+lukafernandes@users.noreply.github.com>
2024-06-09 20:45:46 -0300

    Aula 1

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,5 +1,24 @@
-﻿using ScreenSound.Menus;
+﻿using ScreenSound.Banco;
+using ScreenSound.Menus;
 using ScreenSound.Modelos;
+
+try
+{
+    var artistaDAL = new ArtistaDAL();
+    artistaDAL.Adicionar(new Artista("Foo Fighters", "Foo Fighters é uma banda de rock alternativo americana formada por Dave Grohl em 1995."));
+    var listaArtistas = artistaDAL.Listar();
+
+    foreach ( var artista in listaArtistas)
+    {
+        Console.WriteLine(artista);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+return;
 
 Artista ira = new Artista("Ira!", "Banda Ira!");
 Artista beatles = new("The Beatles", "Banda The Beatles");
