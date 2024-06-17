479119ae84e800719c7501a5bf30c4c7491d8bdb
Luka Fernandes <150366532+lukafernandes@users.noreply.github.com>
2024-06-17 20:46:38 -0300

    Aula 3

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using ScreenSound.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScreenSound.Banco
+namespace ScreenSound.Banco;
+internal class DAL<T> where T : class
 {
-    internal class DAL
+    public readonly ScreenSoundContext context;
+
+    public DAL(ScreenSoundContext context)
     {
+        this.context = context;
+    }
+
+    public IEnumerable<T> Listar()
+    {
+        return context.Set<T>().ToList();
+    }
+    public void Adicionar(T objeto)
+    {
+        context.Set<T>().Add(objeto);
+        context.SaveChanges();
+    }
+    public void Atualizar(T objeto)
+    {
+        context.Set<T>().Update(objeto);
+        context.SaveChanges();
+    }
+    public void Deletar(T objeto)
+    {
+        context.Set<T>().Remove(objeto);
+        context.SaveChanges();
+    }
+
+    public T? RecuperarPor (Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
     }
 }
